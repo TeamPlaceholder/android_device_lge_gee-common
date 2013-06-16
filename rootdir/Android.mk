7bda05a542f413adc34814cd9ecb7f2cbe31c2e1
xboxfanj <xboxfanj@msn.com>
2013-06-16 09:47:04 -0500

    Make sure ueventd goes to root rather than etc, this should make it go past splash
    Change-Id: If7768f8cd905a151ea894c7f0d8c7855d899abc1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -104,6 +104,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.mdm_links.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
@@ -115,11 +123,4 @@ LOCAL_MODULE       := init.qcom.modem_links.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 include $(BUILD_PREBUILT)
