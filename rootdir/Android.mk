1d1ba10bf829ba7cba6375dd7893afdc4737ee02
Jake Weinstein <xboxfanj@msn.com>
2013-10-14 17:01:08 -0500

    Fix last commit

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -253,10 +253,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.sdio.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.sdio.sh
-include $(BUILD_PREBUILT)
 endif
